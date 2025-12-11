48b1217eb8150444f68620bbda24c0d4811b9126
Mohammad Rezaei <mhdrezaei990@gmail.com>
2025-12-12 00:06:36 +0330

    03-show-status-section-and-layout

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,19 @@
+"use client";
+
+import { useState } from "react";
+import { StatusHeader } from "./components/StatusHeader";
+import { StatusContent } from "./components/StatusContent";
+
 export default function DashboardPage() {
+  const [tab, setTab] = useState("general");
+
   return (
-    <>
-      <div>dahsboard</div>
-    </>
+    <div className="space-y-6">
+      {/* Header with tabs */}
+      <StatusHeader value={tab} onChange={setTab} />
+
+      {/* Content under the header */}
+      <StatusContent currentTab={tab} />
+    </div>
   );
 }
